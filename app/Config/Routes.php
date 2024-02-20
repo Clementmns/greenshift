281e83e0387b824bfc8805bef5c526f1239521dd
Titouan Lahchiouach <lahchiouach.titouan@gmail.com>
2024-02-20 14:50:45 +0100

    ajout des routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,5 @@ $routes->get('test', 'Home::dudule');
 $routes->get('etu', 'Etudiants::index');
 $routes->get('grp', 'Groupes::index');
 $routes->get('grpchoose', 'Groupes::choose');
+$routes->get('login', 'Auth::index');
+$routes->get('register', 'Auth::register');
