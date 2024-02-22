c206f2b0d9f21fce42b31d3a8955a44e8b5fbc05
Omnès Clément <clementgoni@gmail.com>
2024-02-22 11:22:22 +0100

    Gestion du routage

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,12 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('login', 'Auth::index');
-$routes->get('register', 'Auth::register');
-$routes->get('etu', 'Etudiants::index');
-$routes->get('grp', 'Groupes::index');
-$routes->get('grpchoose', 'Groupes::choose');
+// $routes->get('/', 'Home::index');
+// $routes->get('etu', 'Etudiants::index');
+// $routes->get('grp', 'Groupes::index');
+// $routes->get('grpchoose', 'Groupes::choose');
 
 $routes->group('', ['filter' => 'AuthCheck'], function ($routes) {
    $routes->get('/dashboard', 'Dashboard::index');
