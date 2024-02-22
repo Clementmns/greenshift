25b5576e64c4ae996ea3317275e37d9d889d90f1
Titouan Lahchiouach <lahchiouach.titouan@gmail.com>
2024-02-22 11:00:02 +0100

    création du formulaire de connexion

--- app/Views/auth/Login.php
+++ app/Views/auth/Login.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Greenshift - Connexion</title>
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>css/style.css">
 </head>
+
 <body>
-    <h1>
-        hello
-    </h1>
+    <form action="" method="post">
+        <?=csrf_field();?>
+        <div>
+            <label for="">Pseudo</label>
+            <input type="text" name="pseudo" placeholder="Pseudo here">
+        </div>
+        <div>
+            <label for="">Password</label>
+            <input type="password" name="password" placeholder="Password here">
+        </div>
+        <div>
+            <input type="submit" value="Sign In">
+        </div>
+    </form>
 </body>
+
 </html>
