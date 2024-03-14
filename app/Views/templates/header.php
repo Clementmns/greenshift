dd37587cc10dbafbb5e5ace807131305947e73f6
Titouan Lahchiouach <lahchiouach.titouan@gmail.com>
2024-03-14 15:53:56 +0100

    changements ?

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/css/output.css">
     <title>Document</title>
-    <script src="<?= base_url(); ?>/js/jquery/dist/jquery.min.js"></script>
+    <script src="<?= base_url(); ?>js/jquery/dist/jquery.min.js"></script>
 </head>
 
 <body class="bg-gray-100">
