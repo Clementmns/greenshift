d72dc464d5c06bc70827133c4d8f2074d4e9aabe
makelia <makelia.mmi@gmail.com>
2024-03-22 12:32:48 +0100

    navbar

---
+++ app/Views/templates/navbar.php
@@ -0,0 +1,9 @@
+<nav class="fixed bottom-0 left-0 w-full bg-white shadow z-50">
+    <ul class="flex justify-around py-3">
+        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/followed.png" alt="Icone 1" class="w-6 h-6"></a></li>
+        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/friend.png" alt="Icone 2" class="w-6 h-6"></a></li>
+        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/home.png" alt="Icone 3" class="w-6 h-6"></a></li>
+        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/badge.png" alt="Icone 4" class="w-6 h-6"></a></li>
+        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/shop.png" alt="Icone 5" class="w-6 h-6"></a></li>
+    </ul>
+</nav>
