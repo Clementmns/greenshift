ce189863a19e5ccfe5f3f7f3bcd55d45d461c591
Titouan Lahchiouach <lahchiouach.titouan@gmail.com>
2024-03-25 16:58:30 +0100

    Navbar active

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -4,10 +4,10 @@ echo view("templates/notification");
 ?>
 
 
-<div class="home box"><?php echo view("goals/goalsweek", $goals); ?></div>
-<div class="ranking box hidden"><?php echo view("dashboard/classement"); ?></div>
-<div class="badges box hidden"><?php echo view("badges/index"); ?></div>
-<div class="shop box hidden"><?php echo view("boutique/index"); ?></div>
+<div class="container home box"><?php echo view("goals/goalsweek", $goals); ?></div>
+<div class="container ranking box hidden"><?php echo view("dashboard/classement"); ?></div>
+<div class="container badges box hidden"><?php echo view("badges/index"); ?></div>
+<div class="container shop box hidden"><?php echo view("boutique/index"); ?></div>
 
 <?php
 echo view("templates/footer");
--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,10 +1,10 @@
 <nav class="fixed bottom-0 left-0 w-full  z-[43]">
     <div class="box rounded-t-3xl rounded-b-none p-2 ">
         <ul class="flex justify-around">
-            <li class="icon-button p-3 m-0 border-b-2 border-primary-400"><img src="<?= base_url("assets/icons/maison.svg"); ?>" alt="Icone 5" class="w-6 h-6 object-cover"></a></li>
-            <li class="icon-button p-3 m-0 "><img src="<?= base_url("assets/icons/trophee.svg"); ?>" alt="Icone 5" class="w-6 h-6 object-cover"></a></li>
-            <li class="icon-button p-3 m-0 "><img src="<?= base_url("assets/icons/badge.svg"); ?>" alt="Icone 5" class="w-6 h-6 object-cover"></a></li>
-            <li class="icon-button p-3 m-0 "><img src="<?= base_url("assets/icons/panier.svg"); ?>" alt="Icone 5" class="w-6 h-6 object-cover"></a></li>
+            <li class="icon-button p-3 m-0 border-b-2 border-primary-400 translate-y-[-7px] " data-icon="home"><img src="<?= base_url("assets/icons/maison.svg"); ?>" alt="Icone 5" class="w-6 h-6 object-cover"></a></li>
+            <li class="icon-button p-3 m-0" data-icon="classement"><img src="<?= base_url("assets/icons/trophee.svg"); ?>" alt="Icone 5" class="w-6 h-6 object-cover"></a></li>
+            <li class="icon-button p-3 m-0" data-icon="badges"><img src="<?= base_url("assets/icons/badge.svg"); ?>" alt="Icone 5" class="w-6 h-6 object-cover"></a></li>
+            <li class="icon-button p-3 m-0" data-icon="shop"><img src="<?= base_url("assets/icons/panier.svg"); ?>" alt="Icone 5" class="w-6 h-6 object-cover"></a></li>
         </ul>
     </div>
 </nav>
@@ -21,5 +21,24 @@
 
         // Lift the clicked icon
         $(this).css('transform', 'translateY(-7px)');
+
+        let attribut = $(this).attr('data-icon');
+
+        $('.container').fadeOut(200);
+        setTimeout(() => {
+
+            if (attribut == "home") {
+                $('.home').fadeIn(200);
+            } else if (attribut == "classement") {
+                $('.ranking').fadeIn(200);
+            } else if (attribut == "badges") {
+                $('.badges').fadeIn(200);
+            } else {
+                $('.shop').fadeIn(200);
+            }
+        }, 200);
+
+        // Show the corresponding page based on the clicked icon
+
     });
 </script>
