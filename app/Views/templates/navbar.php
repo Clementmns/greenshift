37b84ec336c3020410c2c2d2d48b625dad84a3ab
makelia <makelia.mmi@gmail.com>
2024-03-25 11:38:39 +0100

    navbar

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,9 +1,10 @@
-<nav class="fixed bottom-0 left-0 w-full bg-white shadow z-50">
-    <ul class="flex justify-around py-3">
-        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/followed.png" alt="Icone 1" class="w-6 h-6"></a></li>
-        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/friend.png" alt="Icone 2" class="w-6 h-6"></a></li>
-        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/home.png" alt="Icone 3" class="w-6 h-6"></a></li>
-        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/badge.png" alt="Icone 4" class="w-6 h-6"></a></li>
-        <li><a href="#"><img src="/S4/greenshift/public/assets/icons/shop.png" alt="Icone 5" class="w-6 h-6"></a></li>
+<nav class="fixed bottom-0 left-0 w-full  z-50">
+    <div class="box  m-5">
+    <ul class="flex justify-around">
+        <li><a href="<?= base_url("boutique"); ?>"><img src="<?= base_url("assets/icons/badge.svg"); ?>" alt="Icone 5" class="w-8 h-8 object-cover"></a></li>
+        <li><a href="<?= base_url("dashboard/classement"); ?>"><img src="<?= base_url("assets/icons/trophee.svg"); ?>" alt="Icone 5" class="w-8 h-8 object-cover"></a></li>
+        <li><a href="<?= base_url("dashboard"); ?>"><img src="<?= base_url("assets/icons/maison.svg"); ?>" alt="Icone 5" class="w-8 h-8 object-cover"></a></li>
+        <li><a href="<?= base_url("boutique"); ?>"><img src="<?= base_url("assets/icons/panier.svg"); ?>" alt="Icone 5" class="w-8 h-8 object-cover"></a></li>
     </ul>
+    </div>
 </nav>
