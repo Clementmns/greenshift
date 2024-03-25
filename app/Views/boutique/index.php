dcc3ed4be2eaaef392692ce8ec63c8fdb7feade5
makelia <makelia.mmi@gmail.com>
2024-03-25 16:40:33 +0100

    changements

--- app/Views/boutique/index.php
+++ app/Views/boutique/index.php
@@ -43,14 +43,6 @@
             <select name="favorite_badges[]" multiple>
                 <?php foreach ($userBadges as $badge) : ?>
                     <option value="<?php echo $badge['id_badge']; ?>"><?php echo $badge['title']; ?></option>
-        <form action="<?php echo base_url('boutique/buyBadges'); ?>" method="post" class="text-right">
-            <div class="grid grid-cols-3 gap-4">
-                <?php foreach ($allBadges as $badge): ?>
-                    <div class="flex flex-col items-center">
-                        <img src="<?php echo $badge['link']; ?>" alt="<?php echo $badge['title']; ?>" class="w-20 h-20 mb-2">
-                        <span class="font-semibold"><?php echo $badge['title']; ?></span>
-                        <input type="checkbox" name="selected_badges[]" value="<?php echo $badge['id_badge']; ?>" class="mt-2">
-                    </div>
                 <?php endforeach; ?>
             </div>
             <button type="submit" class="bg-primary-500 text-white px-4 py-2 rounded-md hover:bg-primary-700 mt-4">Acheter </button>
