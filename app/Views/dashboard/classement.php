0916311bcea7c09a12b1934ffce1b73d0f7e61d9
Omnès Clément <clementgoni@gmail.com>
2024-03-25 12:58:36 +0100

    Changements z-index

--- app/Views/dashboard/classement.php
+++ app/Views/dashboard/classement.php
@@ -6,7 +6,7 @@ $friends = ["rankingFriend" => $rankingFriend];
 $world = ["rankingFriend" => $rankingWorld];
 ?>
 
-<div id="popup" class="overflow-y-auto hidden fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-start">
+<div id="popup" class="overflow-y-auto hidden fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-start z-50">
    <div class="bg-white p-4 rounded-md w-[90%] mt-44 min-h-20 flex flex-col justify-center max-h-[50vh]">
       <?php
       echo view("relation/search");
