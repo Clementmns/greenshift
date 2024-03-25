5eabae4a466dc05aa5ca06a553ee8aa2f3245c4c
Omnès Clément <clementgoni@gmail.com>
2024-03-25 16:29:02 +0100

    Dashboard

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -5,9 +5,9 @@ echo view("templates/notification");
 
 
 <div class="home box"><?php echo view("goals/goalsweek", $goals); ?></div>
-<div class="ranking box"><?php echo view("dashboard/classement"); ?></div>
-<div class="badges box"><?php echo view("badges/index"); ?></div>
-<div class="shop box"><?php echo view("boutique/index"); ?></div>
+<div class="ranking box hidden"><?php echo view("dashboard/classement"); ?></div>
+<div class="badges box hidden"><?php echo view("badges/index"); ?></div>
+<div class="shop box hidden"><?php echo view("boutique/index"); ?></div>
 
 <?php
 echo view("templates/footer");
